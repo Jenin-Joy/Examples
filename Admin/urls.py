9c0642693208edf254e8385e7f7e6b3633505e17
Jenin-Joy <jeninjoy21@gmail.com>
2025-02-01 10:46:32 +0530

    Select Dropdown

--- Admin/urls.py
+++ Admin/urls.py
@@ -4,4 +4,8 @@ from Admin import views
 app_name="Admin"
 urlpatterns = [
     path("category/",views.category,name="category"),
+    path("deletecategory/<int:id>",views.deletecategory,name="deletecategory"),
+
+    path('search/',views.search,name="search"),
+    path('ajaxcategory/',views.ajaxcategory,name="ajaxcategory"),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from Admin.models import *
+from django.http import JsonResponse
 # Create your views here.
 
 def getdata(parent):
@@ -21,3 +22,20 @@ def category(request):
             tbl_category.objects.create(category_name=request.POST.get("txt_category"))
         return redirect("Admin:category")
     return render(request, "Admin/Category.html", {"category": category})
+
+def deletecategory(request, id):
+    tbl_category.objects.get(id=id).delete()
+    return redirect("Admin:category")
+
+def search(request):
+    cat = tbl_category.objects.filter(parent=None)
+    return render(request, "Admin/Search.html", {"cat": cat})
+
+# def ajaxcategory(request):
+#     subcat = tbl_category.objects.filter(parent=request.GET.get("did"))
+#     return render(request, "Admin/AjaxCategory.html", {"subcat": subcat})
+
+def ajaxcategory(request):
+    did = request.GET.get("did")
+    subcategories = tbl_category.objects.filter(parent=did).values("id", "category_name")  # Assuming `parent_id` for hierarchy
+    return JsonResponse(list(subcategories), safe=False)
